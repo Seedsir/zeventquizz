63b78537acecd9d8e2e2438635c2d9fa2cf1fb86
bastien <bastien.esposito@linxo.com>
2022-10-07 17:24:24 +0200

    comment quizz routes

--- model/quizz/routes.py
+++ model/quizz/routes.py
@@ -1,28 +1,27 @@
-from flask import Blueprint, request
-
-from model.quizz.manager import QuizzManager
-
-app = Blueprint("quizz_app", __name__)
-
-manager = QuizzManager()
-
-
-@app.route("/quizz", methods=["GET"])
-def get_all():
-    return manager.get_all()
-
-
-@app.route("/quizz", methods=["POST"])
-def create_answer():
-    body = request.get_json()
-    return manager.create(body)
-
-
-@app.route("/quizz/<answer_id>", methods=["GET"])
-def get_answer(answer_id):
-    return manager.get(answer_id)
-
-
-@app.route("/quizz/<answer_id>", methods=["DELETE"])
-def delete_answer(answer_id):
-    return manager.delete(answer_id)
+#TODO Work in progress
+# from flask import Blueprint, request
+#
+# app = Blueprint("quizz_app", __name__)
+#
+# manager = QuizzManager()
+#
+#
+# @app.route("/quizz", methods=["GET"])
+# def get_all():
+#     return manager.get_all()
+#
+#
+# @app.route("/quizz", methods=["POST"])
+# def create_answer():
+#     body = request.get_json()
+#     return manager.create(body)
+#
+#
+# @app.route("/quizz/<answer_id>", methods=["GET"])
+# def get_answer(answer_id):
+#     return manager.get(answer_id)
+#
+#
+# @app.route("/quizz/<answer_id>", methods=["DELETE"])
+# def delete_answer(answer_id):
+#     return manager.delete(answer_id)
