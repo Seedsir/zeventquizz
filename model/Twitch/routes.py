576ae8273d48afc6577914d5bf165bad95ba3da2
bastien <bastien.esposito@linxo.com>
2023-02-03 00:02:12 +0100

    Gestion de l'OAuth

--- model/Twitch/routes.py
+++ model/Twitch/routes.py
@@ -1,21 +1,24 @@
 from flask import Blueprint, request, jsonify
 from model.Twitch.oauth_twitch import TwitchAuth
+
 app = Blueprint("twitch", __name__)
 
 
 @app.route("/acceuil", methods=['GET'])
 def acceuil():
-    return jsonify(TwitchAuth.create_user())
+    return f'Hello world'
+
 
 @app.route("/login", methods=['GET'])
-def login(scope="user:read:email"):
-    return TwitchAuth.do_login(scope)
+def login():
+    return TwitchAuth.do_login()
 
 
 @app.route("/authorisation_code", methods=['GET'])
 def get_token():
     code = request.args['code']
     return TwitchAuth.get_token(code)
+
 
 @app.route("/refresh_token", methods=['GET'])
 def get_refresh_token():
--- model/users/routes.py
+++ model/users/routes.py
@@ -1,7 +1,13 @@
-from flask import Blueprint
+from http.client import HTTPException
+
+import requests
+from flask import Blueprint, request, abort
 
 from model.users.user import User
 from flask import jsonify
+
+from utils.constants import CLIENT_ID
+
 app = Blueprint("users", __name__)
 
 
@@ -19,3 +25,19 @@ def get_user(username):
 @app.route("/users/<username>", methods=["DELETE"])
 def delete_user(username):
     return User.delete_user_username(username)
+
+
+@app.route("/users/my_profile", methods=["GET"])
+def get_profile():
+    access_token = request.headers.get('token')
+    if access_token is None:
+        return abort(403)
+    data = {
+        "Authorization": f"Bearer {access_token}",
+        "Client-Id": CLIENT_ID
+    }
+    req = requests.get('https://api.twitch.tv/helix/users', headers=data)
+    if req.status_code != 200:
+        return abort(401)
+    user_id = req.json()["data"][0].get("id")
+    return jsonify(User.get_user_by_id_twitch(user_id).render())
